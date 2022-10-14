f5f25a6eaf9c791fa3f36df740de3ac2a21328c8
minyeong <109118029+Ygzekt@users.noreply.github.com>
2022-10-14 16:26:04 +0900

    Add files via upload

---
+++ aaaa/src/aaaa/ArrayList.java
@@ -0,0 +1,18 @@
+package aaaa;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		int[] arr = { 3, 2, 1, 5, 1 };         // 최대값 초기값 세팅       
+		int max = arr[0];         // 최대값 구하기       
+		for (int num : arr) {            
+			if (num > max) {                
+				max = num;            
+				}        
+			}         
+		// 최대값 출력       
+		System.out.println(max);
+		
+	}
+
+}
